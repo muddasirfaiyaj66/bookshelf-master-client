a260e123bb0ae9f20ce5f56e63af667ab6f98903
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2023-11-06 17:01:40 +0600

    complete auth provider setup

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,14 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext()
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    return (
+       <AuthContext.Provider>
+        {children}
+       </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
